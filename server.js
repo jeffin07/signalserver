869631fe10f8e80f421e623db0afeb39511b01f4
Jeffin Francis <francisjeffin07@gmail.com>
2017-10-23 17:03:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var yetify = require('yetify'),
     config = require('getconfig'),
     fs = require('fs'),
     sockets = require('./sockets'),
-    port = parseInt(process.env.PORT || config.server.port, 10),
+    port = parseInt(8888 || config.server.port, 8888),
     server_handler = function (req, res) {
         res.writeHead(404);
         res.end();
