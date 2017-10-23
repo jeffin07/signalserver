2174cb1a985432e5cfc92e263721409166041c7a
Jeffin Francis <francisjeffin07@gmail.com>
2017-10-23 17:30:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,20 @@ var yetify = require('yetify'),
     server = null;
 
 // Create an http(s) server instance to that socket.io can listen to
-if (config.server.secure) {
+var express = require('express');
+var app     = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
+/*if (config.server.secure) {
     server = require('https').Server({
         key: fs.readFileSync(config.server.key),
         cert: fs.readFileSync(config.server.cert),
@@ -21,7 +34,7 @@ if (config.server.secure) {
 } else {
     server = require('http').Server(server_handler);
 }
-server.listen(port);
+server.listen(port);*/
 
 sockets(server, config);
 
